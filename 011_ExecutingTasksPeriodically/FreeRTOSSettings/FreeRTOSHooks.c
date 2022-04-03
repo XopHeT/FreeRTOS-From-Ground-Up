b6b6ea4221151cdd03011e60443c248e47d0c40d
oos <oos@ektos.net>
2022-04-03 18:08:48 +0300

    Now FreeRTOS won't be built as a library. It doesn't work as I hoped

--- 011_ExecutingTasksPeriodically/FreeRTOSSettings/FreeRTOSHooks.c
+++ 011_ExecutingTasksPeriodically/FreeRTOSSettings/FreeRTOSHooks.c
@@ -26,6 +26,8 @@ choice.  See http://www.freertos.org/a00111.html for an explanation. */
  */
 void vApplicationMallocFailedHook(void);
 
+void vApplicationIdleHook(void);
+
 void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
 
 void vApplicationTickHook(void);
@@ -70,6 +72,40 @@ void vApplicationMallocFailedHook(void) {
   fragmented).  See http://www.freertos.org/a00111.html for more
   information. */
   vAssertCalled(__LINE__, __FILE__);
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook(void) {
+  /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+  to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+  task.  It is essential that code added to this hook function never attempts
+  to block in any way (for example, call xQueueReceive() with a block time
+  specified, or call vTaskDelay()).  If application tasks make use of the
+  vTaskDelete() API function to delete themselves then it is also important
+  that vApplicationIdleHook() is permitted to return to its calling function,
+  because it is the responsibility of the idle task to clean up memory
+  allocated by the kernel to any task that has since deleted itself. */
+
+  /* Uncomment the following code to allow the trace to be stopped with any
+  key press.  The code is commented out by default as the kbhit() function
+  interferes with the run time behaviour. */
+  /*
+    if( _kbhit() != pdFALSE )
+    {
+      if( xTraceRunning == pdTRUE )
+      {
+        vTraceStop();
+        prvSaveTraceFile();
+        xTraceRunning = pdFALSE;
+      }
+    }
+  */
+
+#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY != 1)
+  {
+  }
+#endif
 }
 
 /*-----------------------------------------------------------*/
