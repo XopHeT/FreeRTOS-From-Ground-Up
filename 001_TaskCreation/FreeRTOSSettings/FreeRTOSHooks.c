8d69dc0eeaa33e85538178d4442407337234ae4e
oos <oos@ektos.net>
2022-04-03 19:09:28 +0300

    Now minimal FreeRTOS configuration is used for each example. Idle task example added

--- 001_TaskCreation/FreeRTOSSettings/FreeRTOSHooks.c
+++ 001_TaskCreation/FreeRTOSSettings/FreeRTOSHooks.c
@@ -6,34 +6,9 @@ use a callback function to optionally provide the memory required by the idle
 and timer tasks.  This is the stack that will be used by the timer task.  It is
 declared here, as a global, so it can be checked by a test that is implemented
 in a different file. */
-StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
-
-/* Notes if the trace is running or not. */
-static BaseType_t xTraceRunning = pdTRUE;
-
-/* This demo uses heap_5.c, and these constants define the sizes of the regions
-that make up the total heap.  heap_5 is only used for test and example purposes
-as this demo could easily create one large heap region instead of multiple
-smaller heap regions - in which case heap_4.c would be the more appropriate
-choice.  See http://www.freertos.org/a00111.html for an explanation. */
-#define mainREGION_1_SIZE  10801
-#define mainREGION_2_SIZE  29905
-#define mainREGION_3_SIZE  6007
-
-/*
- * Prototypes for the standard FreeRTOS application hook (callback) functions
- * implemented within this file.  See http://www.freertos.org/a00016.html .
- */
-void vApplicationMallocFailedHook(void);
-
-void vApplicationIdleHook(void);
+StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
 
 void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
-
-void vApplicationTickHook(void);
-
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
-        uint32_t *pulIdleTaskStackSize);
 
 void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer,
         uint32_t *pulTimerTaskStackSize);
@@ -43,70 +18,6 @@ void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackT
  * This function will simply overwrite any trace files that already exist.
  */
 static void prvSaveTraceFile(void);
-
-/*
- * This demo uses heap_5.c, so start by defining some heap regions.  It is not
- * necessary for this demo to use heap_5, as it could define one large heap
- * region.  Heap_5 is only used for test and example purposes.  See
- * http://www.freertos.org/a00111.html for an explanation.
- */
-static void prvInitialiseHeap(void);
-
-/*
- * Performs a few sanity checks on the behaviour of the vPortGetHeapStats()
- * function.
- */
-static void prvExerciseHeapStats(void);
-
-void vApplicationMallocFailedHook(void) {
-  /* vApplicationMallocFailedHook() will only be called if
-  configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-  function that will get called if a call to pvPortMalloc() fails.
-  pvPortMalloc() is called internally by the kernel whenever a task, queue,
-  timer or semaphore is created.  It is also called by various parts of the
-  demo application.  If heap_1.c, heap_2.c or heap_4.c is being used, then the
-  size of the	heap available to pvPortMalloc() is defined by
-  configTOTAL_HEAP_SIZE in FreeRTOSConfig.h, and the xPortGetFreeHeapSize()
-  API function can be used to query the size of free heap space that remains
-  (although it does not provide information on how the remaining heap might be
-  fragmented).  See http://www.freertos.org/a00111.html for more
-  information. */
-  vAssertCalled(__LINE__, __FILE__);
-}
-
-/*-----------------------------------------------------------*/
-
-void vApplicationIdleHook(void) {
-  /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-  to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-  task.  It is essential that code added to this hook function never attempts
-  to block in any way (for example, call xQueueReceive() with a block time
-  specified, or call vTaskDelay()).  If application tasks make use of the
-  vTaskDelete() API function to delete themselves then it is also important
-  that vApplicationIdleHook() is permitted to return to its calling function,
-  because it is the responsibility of the idle task to clean up memory
-  allocated by the kernel to any task that has since deleted itself. */
-
-  /* Uncomment the following code to allow the trace to be stopped with any
-  key press.  The code is commented out by default as the kbhit() function
-  interferes with the run time behaviour. */
-  /*
-    if( _kbhit() != pdFALSE )
-    {
-      if( xTraceRunning == pdTRUE )
-      {
-        vTraceStop();
-        prvSaveTraceFile();
-        xTraceRunning = pdFALSE;
-      }
-    }
-  */
-
-#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY != 1)
-  {
-  }
-#endif
-}
 
 /*-----------------------------------------------------------*/
 
@@ -124,32 +35,7 @@ void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
 
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook(void) {
-  /* This function will be called by each tick interrupt if
-  configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
-  added here, but the tick hook is called from an interrupt context, so
-  code must not attempt to block, and only the interrupt safe FreeRTOS API
-  functions can be used (those that end in FromISR()). */
-
-#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY != 1)
-  {
-  }
-#endif /* mainCREATE_SIMPLE_BLINKY_DEMO_ONLY */
-}
-
-/*-----------------------------------------------------------*/
-
-void vApplicationDaemonTaskStartupHook(void) {
-  /* This function will be called once only, when the daemon task starts to
-  execute	(sometimes called the timer task).  This is useful if the
-  application includes initialisation code that would benefit from executing
-  after the scheduler has been started. */
-}
-
-/*-----------------------------------------------------------*/
-
 void vAssertCalled(unsigned long ulLine, const char *const pcFileName) {
-  static BaseType_t xPrinted = pdFALSE;
   volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
 
   /* Called if an assertion passed to configASSERT() fails.  See
@@ -162,14 +48,6 @@ void vAssertCalled(unsigned long ulLine, const char *const pcFileName) {
 
   taskENTER_CRITICAL();
   {
-    /* Stop the trace recording. */
-    if (xPrinted == pdFALSE) {
-      xPrinted = pdTRUE;
-      if (xTraceRunning == pdTRUE) {
-        prvSaveTraceFile();
-      }
-    }
-
     /* You can step out of this function to debug the assertion by using
     the debugger to set ulSetToNonZeroInDebuggerToContinue to a non-zero
     value. */
@@ -186,110 +64,6 @@ void vAssertCalled(unsigned long ulLine, const char *const pcFileName) {
 static void prvSaveTraceFile(void) {
 }
 
-
-static void prvInitialiseHeap(void) {
-  /* The Windows demo could create one large heap region, in which case it would
-  be appropriate to use heap_4.  However, purely for demonstration purposes,
-  heap_5 is used instead, so start by defining some heap regions.  No
-  initialisation is required when any other heap implementation is used.  See
-  http://www.freertos.org/a00111.html for more information.
-
-  The xHeapRegions structure requires the regions to be defined in start address
-  order, so this just creates one big array, then populates the structure with
-  offsets into the array - with gaps in between and messy alignment just for test
-  purposes. */
-  static uint8_t ucHeap[configTOTAL_HEAP_SIZE];
-  volatile uint32_t ulAdditionalOffset = 19; /* Just to prevent 'condition is always true' warnings in configASSERT(). */
-  HeapStats_t xHeapStats;
-  const HeapRegion_t xHeapRegions[] = {
-          /* Start address with dummy offsets						Size */
-          {ucHeap + 1,                                          mainREGION_1_SIZE},
-          {ucHeap + 15 + mainREGION_1_SIZE,                     mainREGION_2_SIZE},
-          {ucHeap + 19 + mainREGION_1_SIZE + mainREGION_2_SIZE, mainREGION_3_SIZE},
-          {NULL, 0}};
-
-  /* Sanity check that the sizes and offsets defined actually fit into the
-  array. */
-  configASSERT(
-          (ulAdditionalOffset + mainREGION_1_SIZE + mainREGION_2_SIZE + mainREGION_3_SIZE) < configTOTAL_HEAP_SIZE);
-
-  /* Prevent compiler warnings when configASSERT() is not defined. */
-  (void)ulAdditionalOffset;
-
-  /* The heap has not been initialised yet so expect stats to all be zero. */
-  vPortGetHeapStats(&xHeapStats);
-
-  vPortDefineHeapRegions(xHeapRegions);
-
-  /* Sanity check vTaskGetHeapStats(). */
-  prvExerciseHeapStats();
-}
-
-/*-----------------------------------------------------------*/
-
-static void prvExerciseHeapStats(void) {
-  HeapStats_t xHeapStats;
-  size_t xInitialFreeSpace = xPortGetFreeHeapSize(), xMinimumFreeBytes;
-  size_t xMetaDataOverhead, i;
-  void *pvAllocatedBlock;
-  const size_t xArraySize = 5, xBlockSize = 1000UL;
-  void *pvAllocatedBlocks[xArraySize];
-
-  /* Check heap stats are as expected after initialisation but before any
-  allocations. */
-  vPortGetHeapStats(&xHeapStats);
-
-  /* Minimum ever free bytes remaining should be the same as the total number
-  of bytes as nothing has been allocated yet. */
-  configASSERT(xHeapStats.xMinimumEverFreeBytesRemaining == xHeapStats.xAvailableHeapSpaceInBytes);
-  configASSERT(xHeapStats.xMinimumEverFreeBytesRemaining == xInitialFreeSpace);
-
-  /* Nothing has been allocated or freed yet. */
-  configASSERT(xHeapStats.xNumberOfSuccessfulAllocations == 0);
-  configASSERT(xHeapStats.xNumberOfSuccessfulFrees == 0);
-
-  /* Allocate a 1000 byte block then measure what the overhead of the
-  allocation in regards to how many bytes more than 1000 were actually
-  removed from the heap in order to store metadata about the allocation. */
-  pvAllocatedBlock = pvPortMalloc(xBlockSize);
-  configASSERT(pvAllocatedBlock);
-  xMetaDataOverhead = (xInitialFreeSpace - xPortGetFreeHeapSize()) - xBlockSize;
-
-  /* Free the block again to get back to where we started. */
-  vPortFree(pvAllocatedBlock);
-  vPortGetHeapStats(&xHeapStats);
-  configASSERT(xHeapStats.xAvailableHeapSpaceInBytes == xInitialFreeSpace);
-  configASSERT(xHeapStats.xNumberOfSuccessfulAllocations == 1);
-  configASSERT(xHeapStats.xNumberOfSuccessfulFrees == 1);
-
-  /* Allocate blocks checking some stats value on each allocation. */
-  for (i = 0; i < xArraySize; i++) {
-    pvAllocatedBlocks[i] = pvPortMalloc(xBlockSize);
-    configASSERT(pvAllocatedBlocks[i]);
-    vPortGetHeapStats(&xHeapStats);
-    configASSERT(xHeapStats.xMinimumEverFreeBytesRemaining ==
-                 (xInitialFreeSpace - ((i + 1) * (xBlockSize + xMetaDataOverhead))));
-    configASSERT(xHeapStats.xMinimumEverFreeBytesRemaining == xHeapStats.xAvailableHeapSpaceInBytes);
-    configASSERT(xHeapStats.xNumberOfSuccessfulAllocations == (2Ul + i));
-    configASSERT(xHeapStats.xNumberOfSuccessfulFrees == 1); /* Does not increase during allocations. */
-  }
-
-  configASSERT(xPortGetFreeHeapSize() == xPortGetMinimumEverFreeHeapSize());
-  xMinimumFreeBytes = xPortGetFreeHeapSize();
-
-  /* Free the blocks again. */
-  for (i = 0; i < xArraySize; i++) {
-    vPortFree(pvAllocatedBlocks[i]);
-    vPortGetHeapStats(&xHeapStats);
-    configASSERT(xHeapStats.xAvailableHeapSpaceInBytes ==
-                 (xInitialFreeSpace - (((xArraySize - i - 1) * (xBlockSize + xMetaDataOverhead)))));
-    configASSERT(xHeapStats.xNumberOfSuccessfulAllocations == (xArraySize + 1)); /* Does not increase during frees. */
-    configASSERT(xHeapStats.xNumberOfSuccessfulFrees == (2UL + i));
-  }
-
-  /* The minimum ever free heap size should not change as blocks are freed. */
-  configASSERT(xMinimumFreeBytes == xPortGetMinimumEverFreeHeapSize());
-}
 /*-----------------------------------------------------------*/
 
 /* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
--- 005_ChangingTaskPrioritiesInRealTime/main.cpp
+++ 005_ChangingTaskPrioritiesInRealTime/main.cpp
@@ -2,126 +2,26 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define mainREGION_1_SIZE	10801
-#define mainREGION_2_SIZE	29905
-#define mainREGION_3_SIZE	6007
+struct TaskStack {
+  static const BaseType_t stackDepth = configMINIMAL_STACK_SIZE;
+  StackType_t stack[stackDepth];
+  StaticTask_t task;
+};
 
-static void prvExerciseHeapStats( void )
-{
-  HeapStats_t xHeapStats;
-  size_t xInitialFreeSpace = xPortGetFreeHeapSize(), xMinimumFreeBytes;
-  size_t xMetaDataOverhead, i;
-  void *pvAllocatedBlock;
-  const size_t xArraySize = 5, xBlockSize = 1000UL;
-  void *pvAllocatedBlocks[ xArraySize ];
-
-  /* Check heap stats are as expected after initialisation but before any
-  allocations. */
-  vPortGetHeapStats( &xHeapStats );
-
-  /* Minimum ever free bytes remaining should be the same as the total number
-  of bytes as nothing has been allocated yet. */
-  configASSERT( xHeapStats.xMinimumEverFreeBytesRemaining == xHeapStats.xAvailableHeapSpaceInBytes );
-  configASSERT( xHeapStats.xMinimumEverFreeBytesRemaining == xInitialFreeSpace );
-
-  /* Nothing has been allocated or freed yet. */
-  configASSERT( xHeapStats.xNumberOfSuccessfulAllocations == 0 );
-  configASSERT( xHeapStats.xNumberOfSuccessfulFrees == 0 );
-
-  /* Allocate a 1000 byte block then measure what the overhead of the
-  allocation in regards to how many bytes more than 1000 were actually
-  removed from the heap in order to store metadata about the allocation. */
-  pvAllocatedBlock = pvPortMalloc( xBlockSize );
-  configASSERT( pvAllocatedBlock );
-  xMetaDataOverhead = ( xInitialFreeSpace - xPortGetFreeHeapSize() ) - xBlockSize;
-
-  /* Free the block again to get back to where we started. */
-  vPortFree( pvAllocatedBlock );
-  vPortGetHeapStats( &xHeapStats );
-  configASSERT( xHeapStats.xAvailableHeapSpaceInBytes == xInitialFreeSpace );
-  configASSERT( xHeapStats.xNumberOfSuccessfulAllocations == 1 );
-  configASSERT( xHeapStats.xNumberOfSuccessfulFrees == 1 );
-
-  /* Allocate blocks checking some stats value on each allocation. */
-  for( i = 0; i < xArraySize; i++ )
-  {
-    pvAllocatedBlocks[ i ] = pvPortMalloc( xBlockSize );
-    configASSERT( pvAllocatedBlocks[ i ] );
-    vPortGetHeapStats( &xHeapStats );
-    configASSERT( xHeapStats.xMinimumEverFreeBytesRemaining == ( xInitialFreeSpace - ( ( i + 1 ) * ( xBlockSize + xMetaDataOverhead ) ) ) );
-    configASSERT( xHeapStats.xMinimumEverFreeBytesRemaining == xHeapStats.xAvailableHeapSpaceInBytes );
-    configASSERT( xHeapStats.xNumberOfSuccessfulAllocations == ( 2Ul + i ) );
-    configASSERT( xHeapStats.xNumberOfSuccessfulFrees == 1 ); /* Does not increase during allocations. */
-  }
-
-  configASSERT( xPortGetFreeHeapSize() == xPortGetMinimumEverFreeHeapSize() );
-  xMinimumFreeBytes = xPortGetFreeHeapSize();
-
-  /* Free the blocks again. */
-  for( i = 0; i < xArraySize; i++ )
-  {
-    vPortFree( pvAllocatedBlocks[ i ] );
-    vPortGetHeapStats( &xHeapStats );
-    configASSERT( xHeapStats.xAvailableHeapSpaceInBytes == ( xInitialFreeSpace - ( ( ( xArraySize - i - 1 ) * ( xBlockSize + xMetaDataOverhead ) ) ) ) );
-    configASSERT( xHeapStats.xNumberOfSuccessfulAllocations == ( xArraySize + 1 ) ); /* Does not increase during frees. */
-    configASSERT( xHeapStats.xNumberOfSuccessfulFrees == ( 2UL + i ) );
-  }
-
-  /* The minimum ever free heap size should not change as blocks are freed. */
-  configASSERT( xMinimumFreeBytes == xPortGetMinimumEverFreeHeapSize() );
-}
-
-static void  prvInitialiseHeap( void )
-{
-  /* The Windows demo could create one large heap region, in which case it would
-  be appropriate to use heap_4.  However, purely for demonstration purposes,
-  heap_5 is used instead, so start by defining some heap regions.  No
-  initialisation is required when any other heap implementation is used.  See
-  http://www.freertos.org/a00111.html for more information.
-
-  The xHeapRegions structure requires the regions to be defined in start address
-  order, so this just creates one big array, then populates the structure with
-  offsets into the array - with gaps in between and messy alignment just for test
-  purposes. */
-  static uint8_t ucHeap[ configTOTAL_HEAP_SIZE ];
-  volatile uint32_t ulAdditionalOffset = 19; /* Just to prevent 'condition is always true' warnings in configASSERT(). */
-  HeapStats_t xHeapStats;
-  const HeapRegion_t xHeapRegions[] =
-          {
-                  /* Start address with dummy offsets						Size */
-                  { ucHeap + 1,											mainREGION_1_SIZE },
-                  { ucHeap + 15 + mainREGION_1_SIZE,						mainREGION_2_SIZE },
-                  { ucHeap + 19 + mainREGION_1_SIZE + mainREGION_2_SIZE,	mainREGION_3_SIZE },
-                  { NULL, 0 }
-          };
-
-  /* Sanity check that the sizes and offsets defined actually fit into the
-  array. */
-  configASSERT( ( ulAdditionalOffset + mainREGION_1_SIZE + mainREGION_2_SIZE + mainREGION_3_SIZE ) < configTOTAL_HEAP_SIZE );
-
-  /* Prevent compiler warnings when configASSERT() is not defined. */
-  ( void ) ulAdditionalOffset;
-
-  /* The heap has not been initialised yet so expect stats to all be zero. */
-  vPortGetHeapStats( &xHeapStats );
-
-  vPortDefineHeapRegions( xHeapRegions );
-
-  /* Sanity check vTaskGetHeapStats(). */
-  prvExerciseHeapStats();
-}
+TaskStack BlueLed;
+TaskStack RedLed;
+TaskStack GreenLed;
 
 [[noreturn]] void vLEDControllerTask(void *pvParameters);
-TaskHandle_t blue_handle, red_handle, green_handle;
+
 
 int main() {
-  prvInitialiseHeap();
-  xTaskCreate(vLEDControllerTask, "Blue LED controller", 100, // stack size
-          (void *)"B: Blue", 1, &blue_handle);
-  xTaskCreate(vLEDControllerTask, "Red LED controller", 100, // stack size
-          (void *)"R: Red", 1, &red_handle);
-  xTaskCreate(vLEDControllerTask, "Green LED controller", 100, // stack size
-          (void *)"G: Green", 2, &green_handle);
+  xTaskCreateStatic(vLEDControllerTask, "Blue LED controller", BlueLed.stackDepth,
+          (void *)"B: Blue", 1, BlueLed.stack, &BlueLed.task);
+  xTaskCreateStatic(vLEDControllerTask, "Red LED controller", RedLed.stackDepth,
+          (void *)"R: Red", 1, RedLed.stack, &RedLed.task);
+  xTaskCreateStatic(vLEDControllerTask, "Green LED controller", GreenLed.stackDepth,
+          (void *)"G: Green", 2, GreenLed.stack, &GreenLed.task);
 
   vTaskStartScheduler();
 
